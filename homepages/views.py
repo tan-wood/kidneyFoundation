6d83c4269013ace75979b92a8412fe7c175adfae
tannerfunk <88557372+tannerfunk@users.noreply.github.com>
2022-11-29 11:17:39 -0700

    installed requests

--- homepages/views.py
+++ homepages/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
 def indexPageView(request) :
     return render(request,'homepages/index.html')
+
+
+
+# # Create your views here.
+# def index(request):
+#     response=requests.get('https://api.covid19api.com/countries').json()
+#     return render(request,'index.html',{'response':response})
