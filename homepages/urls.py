480d916b49e967764be8e19f0c2bb484fe105713
tannerfunk <88557372+tannerfunk@users.noreply.github.com>
2022-11-29 14:56:45 -0700

    apiconnection

--- homepages/urls.py
+++ homepages/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import indexPageView
+from .views import indexPageView, apiPageView, apiJSONView
 
 urlpatterns = [
-    path("", indexPageView, name="index")    
+    path("", indexPageView, name="index"),
+    path("apitest/", apiPageView, name="apitest" ),
+    path("apiJSON/", apiJSONView, name="apiJSON" ),
+
 ]      
--- homepages/views.py
+++ homepages/views.py
@@ -1,13 +1,40 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
 import requests
+from django.conf import settings
+import json
 
 def indexPageView(request) :
     return render(request,'homepages/index.html')
 
 
+# def apiPageView(request) :
+#     response=requests.get(f'https://api.nal.usda.gov/fdc/v1/foods/search?api_key={settings.API_KEY}&query=Cheddar%20Cheese').json()
+#     print(response)
+#     return render(request,'homepages/apitest.html',{'response':response})
 
-# # Create your views here.
-# def index(request):
-#     response=requests.get('https://api.covid19api.com/countries').json()
-#     return render(request,'index.html',{'response':response})
+
+def apiJSONView(request) :
+    response=requests.get(f'https://api.nal.usda.gov/fdc/v1/foods/search?query=apple&dataType=&pageSize=1&pageNumber=1&sortBy=dataType.keyword&sortOrder=asc&api_key={settings.API_KEY}').json()
+    return JsonResponse(response)
+
+def apiPageView(request) :
+    food_names = {}
+    if 'name' in request.GET:
+        name = request.GET['name']
+        response=requests.get(f'https://api.nal.usda.gov/fdc/v1/foods/search?query={name}&dataType=&pageSize=8&pageNumber=1&sortBy=dataType.keyword&sortOrder=desc&api_key={settings.API_KEY}')
+        data = response.json()
+ 
+        for idx, food in enumerate(data['foods']) :
+            food_names['food_name' + str(idx+1)] = food['description']
+    
+
+    # send it to the database when clicked!
+    # not sure if that's in this view or not.. i think it is
+    # food_name = WHAT BUTTON THEY CLICKED
+    # something = Food.objects.something?
+
+
+    return render (request, 'homepages/apitest.html', { "food_names": 
+    food_names} )
+
