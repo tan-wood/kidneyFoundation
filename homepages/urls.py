5824313bfc743cb6884fe51a1b05a8559e8b5c29
treyjacksonn <113260616+treyjacksonn@users.noreply.github.com>
2022-11-29 11:30:23 -0700

    landing and login url and views

--- homepages/urls.py
+++ homepages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import indexPageView
+from .views import indexPageView, LandingPageView, LoginPageView
 
 urlpatterns = [
-    path("", indexPageView, name="index")    
+    path("", indexPageView, name="index"),
+    path('landing', ) 
 ]      
--- homepages/views.py
+++ homepages/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import requests
 
 def indexPageView(request) :
     return render(request,'homepages/index.html')
 
+def LandingPageView(request):
+    return render(request,'landingpage.html')
 
-
-# # Create your views here.
-# def index(request):
-#     response=requests.get('https://api.covid19api.com/countries').json()
-#     return render(request,'index.html',{'response':response})
+def LoginPageView(request):
+    return render(request, 'login.html')
