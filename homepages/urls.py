f18f0a7e5fd8e491fb1fac5e00a85ca7167269f8
Adam Norton <adam3norton@gmail.com>
2022-11-29 23:58:12 -0700

    Home Page and Base html

--- homepages/urls.py
+++ homepages/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from .views import indexPageView, LandingPageView, LoginPageView, AboutPageView, apiPageView, apiJSONView
 
 urlpatterns = [
-    path("home/", indexPageView, name="landing"),
+    path("home/", indexPageView, name="home"),
     path("login/", LoginPageView, name="login"),
     path("about/", AboutPageView, name="about"),
-        path("apitest/", apiPageView, name="apitest" ),
+    path("apitest/", apiPageView, name="apitest" ),
     path("apiJSON/", apiJSONView, name="apiJSON" ),
+    path("landing/", LandingPageView, name="landing"),
     path("", LandingPageView, name="index")    
-
-]      
+]
