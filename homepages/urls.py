87986bb1c52f24ff7387c9df93862ebbbb1de872
tannerfunk <88557372+tannerfunk@users.noreply.github.com>
2022-11-30 20:24:26 -0700

    workingForm

--- homepages/urls.py
+++ homepages/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("home/", indexPageView, name="landing"),
     path("login/", LoginPageView, name="login"),
     path("about/", AboutPageView, name="about"),
-        path("apitest/", apiPageView, name="apitest" ),
+    path("apitest/", apiPageView, name="apitest" ),
     path("apiJSON/", apiJSONView, name="apiJSON" ),
     path("", LandingPageView, name="index")    
 
